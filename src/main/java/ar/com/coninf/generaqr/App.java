71c98c6ad19eab6d8cdab18545156a500ca4685e
Eduardo Marturano <emartura@boldt.com.ar>
2021-02-25 19:24:30 -0300

    ini 1.0.0

---
+++ src/main/java/ar/com/coninf/generaqr/App.java
@@ -0,0 +1,13 @@
+package ar.com.coninf.generaqr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
